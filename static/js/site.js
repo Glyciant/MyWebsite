960e0cfbd6e4c7e76bdb07519928db469fc956ae
Glyciant <glyciant@gmail.com>
2018-11-27 20:11:36 +0000

    Added New Site Sections

--- static/js/site.js
+++ static/js/site.js
@@ -1,15 +1,65 @@
 $(document).ready(function() {
     $("[data-toggle=\"tooltip\"]").tooltip();
+    $("html, body").css({
+        overflow: "hidden",
+        height: "100%"
+    });
+    $("[data-toggle=\"tooltip\"]").tooltip();
     setTimeout(function() {
         $("#header-image").fadeIn(function() {
             setTimeout(function() {
                 $("#header-content-top").fadeIn(function() {
                     setTimeout(function() {
-                        $("#header-content-middle").fadeIn();
-                        $("#header-content-bottom").fadeIn();
+                        $("#header-content-middle, #header-content-bottom").fadeIn(function() {
+                            $("html, body").css({
+                                overflow: "auto",
+                                height: "auto"
+                            });
+                        });
                     }, 200);
                 });
             }, 200);
         });
     }, 500);
 });
+
+$(document).delegate("#nav-about", "click", function() {
+    $("body").scrollTo("#about", {
+        duration: 1000,
+        ease: "swing"
+    });
+});
+
+$(document).delegate("#nav-projects", "click", function() {
+    $("body").scrollTo("#projects", {
+        duration: 1000,
+        ease: "swing"
+    });
+});
+
+$(document).delegate("#nav-contact", "click", function() {
+    $("body").scrollTo("#contact", {
+        duration: 1000,
+        ease: "swing"
+    });
+});
+
+$(document).delegate("#nav-top", "click", function() {
+    $("body").scrollTo("#header", {
+        duration: 1000,
+        ease: "swing"
+    });
+});
+
+$(document).on("scroll", function() {
+    if ($(this).scrollTop() >= $('#about').position().top) {
+        $("#nav-top").show("slide", {
+            direction: "right"
+        }, 200);
+    }
+    else {
+        $("#nav-top").hide("slide", {
+            direction: "right"
+        }, 200);
+    }
+})
